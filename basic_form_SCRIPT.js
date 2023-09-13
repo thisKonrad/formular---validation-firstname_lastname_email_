430bef7fec565c1327e1d84f41e376b0719b61d1
Konrad von Wittich <127662701+thisKonrad@users.noreply.github.com>
2023-09-13 10:05:20 +0200

    Update basic_form_SCRIPT.js

--- basic_form_SCRIPT.js
+++ basic_form_SCRIPT.js
@@ -1,57 +1,55 @@
-/* :˅::˅: basic formular validation SCRIPT js :˅::˅: */
+/* :: >> basic formular validation SCRIPT js <<  :: */
 
 
 /* get the formular */
-
 let formularBasic = document.querySelector('#formular')
 
 
 /* submit the formular values */
-
 formularBasic.addEventListener('submit', sendForm ); 
+
+
 function sendForm(event){
+
     event.preventDefault()
 
-    formularBasic = event.target;                    /* >> get the formular as event target:: */
-    let formData = new FormData(formularBasic);      /* >> create an new FormData() and knot it to the current formular */
+    /* get the formular as event target */
+    formularBasic = event.target;                    
+    let formData = new FormData(formularBasic);      
     
-    /* remind.. → FormData()Method only works with named HTML elements! */
+    /** remind.. → FormData()Method only works with named HTML elements! 
+    * with FormData() you can append new informations invisible for the user */
+
+    /** append an individual ID(timestamp)  
+    *   in milliseconds */
+    let timestamp = Date.now();
     
-    /* with FormData() you can append new informations invisible for the user */
+    formData.append('timestamp ID: ', timestamp);
 
-    /** append an individual event number for this instance take the current  
-    *   date in milliseconds * Math.random() for an individual number */
-
-    let timebase = new Date();
-    let milSecBase = timebase.getTime();/* ◄ milliseconds*/
-    let xy = Math.floor(Math.random(3.45)* milSecBase);
- 
-    formData.append('event number: ', xy );
-
-    /* ˅ validate email adress function :: dekalred below this interface */
+    /* ˅ validate email adress function */
 
     validMail();
 
     /* append some time informations */
-
     formData.append('submit date: ', new Date().toLocaleDateString())
     formData.append('submit time: ', new Date().toLocaleTimeString())
 
     /* append users href information: */
-
     formData.append('pathName: ', window.location.href);
     
     /** submit all values ...)))
-    *  ↓ ☺ objects in the FormData() interface can be looped with a for of loop */
+    *  ↓ ☺ objects in the FormData() interface can be iterated */
 
     for( let key of formData.keys()){
         console.table(key, formData.get(key));   
     }
-    //optional after confirm :: => formData.reset();
+    /* optional after confirm: => formData.reset(); */
 };
 
 
-/* after html validation :: validate the mail adress again with js and regular expression */
+/** after html validation :: 
+* validate the mail adress again 
+* with js and regular expression */
 
 const mail = document.querySelector('#mail');
 
